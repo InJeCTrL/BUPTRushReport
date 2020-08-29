843b08f1057780ea2e2c37c5d01a76a5a106aed5
InJeCTrL <dap1933@hotmail.com>
2020-08-29 18:04:51 +0800

    version 1.0.0Alpha almost done

---
+++ BUPTRushReport/MainWindow.xaml.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace BUPTRushReport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> UserList = new ObservableCollection<User>();
+        private Timer t_Morning, t_Moon, t_Night;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadCfgFromFile();
+            UserGrid.DataContext = UserList;
+            t_Morning = new Timer(Timedup_Morning);
+            SetTime_Morning();
+            t_Moon = new Timer(Timedup_Moon);
+            SetTime_Moon();
+            t_Night = new Timer(Timedup_Night);
+            SetTime_Night();
+        }
+        /// <summary>
+        /// 从导出文件读取配置
+        /// </summary>
+        private void ReadCfgFromFile()
+        {
+            try
+            {
+                using (FileStream fs = new FileStream("userlist.xml", FileMode.Open))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(ObservableCollection<User>));
+                    UserList = (ObservableCollection<User>)xmlSerializer.Deserialize(fs);
+                    MessageBox.Show("已导入现有配置");
+                }
+            }
+            catch
+            {
+                ;
+            }
+        }
+        /// <summary>
+        /// 达到晨检预定时间
+        /// </summary>
+        /// <param name="obj"></param>
+        private void Timedup_Morning(object obj)
+        {
+            RushList(DateTime.Now, 0);
+            SetTime_Morning(true);
+        }
+        /// <summary>
+        /// 达到午检预定时间
+        /// </summary>
+        /// <param name="obj"></param>
+        private void Timedup_Moon(object obj)
+        {
+            RushList(DateTime.Now, 1);
+            SetTime_Moon(true);
+        }
+        /// <summary>
+        /// 达到晚检预定时间
+        /// </summary>
+        /// <param name="obj"></param>
+        private void Timedup_Night(object obj)
+        {
+            RushList(DateTime.Now, 2);
+            SetTime_Night(true);
+        }
+        /// <summary>
+        /// 过一遍整个列表
+        /// </summary>
+        /// <param name="time">执行时间</param>
+        /// <param name="type">填报类型(早: 0, 午: 1, 晚: 2)</param>
+        private void RushList(DateTime time, int type)
+        {
+            Task.Run(() =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    UserGrid.IsReadOnly = true;
+                });
+                foreach (var item in UserList)
+                {
+                    RushOne(item, time, type);
+                }
+                this.Dispatcher.Invoke(() =>
+                {
+                    UserGrid.IsReadOnly = false;
+                });
+            });
+        }
+        /// <summary>
+        /// 对列表中某一项操作
+        /// </summary>
+        /// <param name="userInfo">用户信息</param>
+        /// <param name="time">执行时间</param>
+        /// <param name="type">填报类型(早: 0, 午: 1, 晚: 2)</param>
+        private void RushOne(User user, DateTime time, int type)
+        {
+            user.Submit(time, type);
+        }
+        /// <summary>
+        /// 设定晨检Rush时间
+        /// </summary>
+        /// <param name="TodayComplete">是否当日已Rush</param>
+        private void SetTime_Morning(bool TodayComplete = false)
+        {
+            DateTime Today = DateTime.Now;
+            // 当日12点后或当日Rush过, 设定次日6点10填报
+            if (Today.Hour >= 12 || TodayComplete)
+            {
+                DateTime Tomorrow = Today.AddDays(1);
+                t_Morning.Change((int)(new DateTime(Tomorrow.Year, Tomorrow.Month, Tomorrow.Day,
+                                    6, 10, 0) - Today).TotalMilliseconds, Timeout.Infinite);
+            }
+            // 在当天填报时间范围内, 立刻填报
+            else if (Today.Hour >= 6 && Today.Hour < 12)
+            {
+                t_Morning.Change(0, Timeout.Infinite);
+            }
+            // 当日6点前, 设定当日6点10填报
+            else
+            {
+                t_Morning.Change((int)(new DateTime(Today.Year, Today.Month, Today.Day,
+                    6, 10, 0) - Today).TotalMilliseconds, Timeout.Infinite);
+            }
+        }
+        /// <summary>
+        /// 设定午检Rush时间
+        /// </summary>
+        /// <param name="TodayComplete">是否当日已Rush</param>
+        private void SetTime_Moon(bool TodayComplete = false)
+        {
+            DateTime Today = DateTime.Now;
+            // 当日17点后或当日Rush过, 设定次日15点10填报
+            if (Today.Hour >= 17 || TodayComplete)
+            {
+                DateTime Tomorrow = Today.AddDays(1);
+                t_Moon.Change((int)(new DateTime(Tomorrow.Year, Tomorrow.Month, Tomorrow.Day,
+                                    15, 10, 0) - Today).TotalMilliseconds, Timeout.Infinite);
+            }
+            // 在当天填报时间范围内, 立刻填报
+            else if (Today.Hour >= 14 && Today.Hour < 17)
+            {
+                t_Moon.Change(0, Timeout.Infinite);
+            }
+            // 当日14点前, 设定当日15点10填报
+            else
+            {
+                t_Moon.Change((int)(new DateTime(Today.Year, Today.Month, Today.Day,
+                    15, 10, 0) - Today).TotalMilliseconds, Timeout.Infinite);
+            }
+        }
+        /// <summary>
+        /// 设定晚检Rush时间
+        /// </summary>
+        /// <param name="TodayComplete">是否当日已Rush</param>
+        private void SetTime_Night(bool TodayComplete = false)
+        {
+            DateTime Today = DateTime.Now;
+            // 当日22点后或当日Rush过, 设定次日19点50填报
+            if (Today.Hour >= 22 || TodayComplete)
+            {
+                DateTime Tomorrow = Today.AddDays(1);
+                t_Night.Change((int)(new DateTime(Tomorrow.Year, Tomorrow.Month, Tomorrow.Day,
+                                    19, 50, 0) - Today).TotalMilliseconds, Timeout.Infinite);
+            }
+            // 在当天填报时间范围内, 立刻填报
+            else if (Today.Hour >= 19 && Today.Hour < 22)
+            {
+                t_Night.Change(0, Timeout.Infinite);
+            }
+            // 当日19点前, 设定当日19点50填报
+            else
+            {
+                t_Night.Change((int)(new DateTime(Today.Year, Today.Month, Today.Day,
+                    19, 50, 0) - Today).TotalMilliseconds, Timeout.Infinite);
+            }
+        }
+        /// <summary>
+        /// 手动进行一次列表Rush
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RushOne_Click(object sender, RoutedEventArgs e)
+        {
+            RushList(DateTime.Now, 0);
+        }
+        /// <summary>
+        /// 导出列表到文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            using (FileStream fs = new FileStream("userlist.xml", FileMode.Create))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(ObservableCollection<User>));
+                xmlSerializer.Serialize(fs, UserList);
+                MessageBox.Show("导出完成");
+            }
+
+        }
+
+    }
+}
---
+++ BUPTRushReport/User.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace BUPTRushReport
+{
+    public class User : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public User()
+        {
+            userid = "学号";
+            userpwd = "密码";
+            Cookies = "";
+            AutoMoon = false;
+            AutoNight = false;
+            Skip = false;
+            Payload1 = "";
+            Payload2 = "";
+            LatestResult_Morning = "";
+            LatestResult_Moon = "";
+            LatestResult_Night = "";
+        }
+        /// <summary>
+        /// 学号
+        /// </summary>
+        public string userid { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string userpwd { get; set; }
+        /// <summary>
+        /// 成功登录Cookies
+        /// </summary>
+        public string Cookies { get; set; }
+        /// <summary>
+        /// 填报页面HTML
+        /// </summary>
+        private string page_Input = "";
+        /// <summary>
+        /// 验证网站、账号并尝试获取Cookies
+        /// </summary>
+        /// <param name="DayTime">执行时间</param>
+        /// <param name="type">填报类型(早: 0, 午: 1, 晚: 2)</param>
+        /// <returns>
+        /// 登录成功: true, 并设置Cookies;
+        /// 登录失败: false, 并设置LatestResult
+        /// </returns>
+        private bool Verify(string DayTime, int type)
+        {
+            bool LoginResult = false;
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://app.bupt.edu.cn/uc/wap/login/check");
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0";
+            // 禁止自动跳转
+            webRequest.AllowAutoRedirect = false;
+            try
+            {
+                using (Stream reqstream = webRequest.GetRequestStream())
+                {
+                    byte[] LoginPayload = Encoding.UTF8.GetBytes("username=" + userid + "&password=" + userpwd);
+                    reqstream.Write(LoginPayload, 0, LoginPayload.Length);
+                    reqstream.Flush();
+                    using (WebResponse webResponse = webRequest.GetResponse())
+                    {
+                        string cookies = webResponse.Headers.Get("Set-Cookie");
+                        using (Stream respstream = webResponse.GetResponseStream())
+                        {
+                            using (StreamReader streamReader = new StreamReader(respstream))
+                            {
+                                string ret = streamReader.ReadToEnd();
+                                // Console.WriteLine(cookies);
+                                var reply = JsonSerializer.Deserialize<Dictionary<string, object>>(ret);
+                                if (reply["e"].ToString() != "0")
+                                {
+                                    Cookies = "";
+                                    switch (type)
+                                    {
+                                        case 0:
+                                            LatestResult_Morning = DayTime + reply["m"].ToString();
+                                            break;
+                                        case 1:
+                                            LatestResult_Moon = DayTime + reply["m"].ToString();
+                                            break;
+                                        case 2:
+                                            LatestResult_Night = DayTime + reply["m"].ToString();
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                }
+                                else
+                                {
+                                    Cookies = cookies;
+                                    LoginResult = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Cookies = "";
+                switch (type)
+                {
+                    case 0:
+                        LatestResult_Morning = DayTime + "网站异常";
+                        break;
+                    case 1:
+                        LatestResult_Moon = DayTime + "网站异常";
+                        break;
+                    case 2:
+                        LatestResult_Night = DayTime + "网站异常";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            finally
+            {
+                if (webRequest != null)
+                {
+                    webRequest.Abort();
+                }
+            }
+            return LoginResult;
+        }
+        /// <summary>
+        /// 检查当日某时段是否已填报
+        /// </summary>
+        /// <param name="type">填报类型(早: 0, 午: 1, 晚: 2)</param>
+        /// <returns>
+        /// 已填报: true;
+        /// 未填报: false
+        /// </returns>
+        private bool CheckSubmitted(int type)
+        {
+            bool flag = false;
+            string checkURL = "";
+            // 晨检
+            if (type == 0)
+            {
+                checkURL = "https://app.bupt.edu.cn/ncov/wap/default/index";
+            }
+            // 午晚检
+            else
+            {
+                checkURL = "https://app.bupt.edu.cn/xisuncov/wap/open-report/index";
+            }
+            WebRequest webRequest = WebRequest.Create(checkURL);
+            webRequest.Headers.Set(HttpRequestHeader.Cookie, Cookies);
+            webRequest.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0";
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                using (Stream stream = webResponse.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        page_Input = reader.ReadToEnd();
+                        // 晨检
+                        if (type == 0)
+                        {
+                            if (page_Input.IndexOf("hasFlag: '1'") != -1)
+                            {
+                                flag = true;
+                            }
+                        }
+                        // 午晚检
+                        else
+                        {
+                            if (page_Input.IndexOf("\"realonly\":true") != -1)
+                            {
+                                flag = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (webRequest != null)
+            {
+                webRequest.Abort();
+            }
+            return flag;
+        }
+        /// <summary>
+        /// 提交填报信息
+        /// </summary>
+        /// <param name="time">执行时间</param>
+        /// <param name="type">填报类型(早: 0, 午: 1, 晚: 2)</param>
+        public void Submit(DateTime time, int type)
+        {
+            string DayTime = time.Year.ToString() + "." +
+                                time.Month.ToString() + "." +
+                                time.Day.ToString() + " ";
+            if (!Skip && 
+                ((type == 1 && AutoMoon) || (type == 2 && AutoNight) || type == 0) && 
+                Verify(DayTime, type))
+            {
+                if (CheckSubmitted(type) == false)
+                {
+                    string submitURL = "";
+                    byte[] Payload = { };
+                    // 晨检
+                    if (type == 0)
+                    {
+                        submitURL = "https://app.bupt.edu.cn/ncov/wap/default/save";
+                        Payload = Encoding.UTF8.GetBytes(Payload1);
+                    }
+                    // 午晚检
+                    else
+                    {
+                        submitURL = "https://app.bupt.edu.cn/xisuncov/wap/open-report/save";
+                        Payload = Encoding.UTF8.GetBytes(Payload2);
+                    }
+                    WebRequest webRequest = WebRequest.Create(submitURL);
+                    webRequest.Method = "POST";
+                    webRequest.ContentType = "application/x-www-form-urlencoded";
+                    webRequest.Headers.Set(HttpRequestHeader.Cookie, Cookies);
+                    webRequest.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0";
+                    using (Stream reqstream = webRequest.GetRequestStream())
+                    {
+                        reqstream.Write(Payload, 0, Payload.Length);
+                        reqstream.Flush();
+                        WebResponse webResponse = webRequest.GetResponse();
+                        webResponse.Dispose();
+                        webResponse.Close();
+                    }
+                    if (webRequest != null)
+                    {
+                        webRequest.Abort();
+                    }
+                    string twiceChk = "";
+                    if (CheckSubmitted(type))
+                    {
+                        twiceChk = "自动填报成功";
+                    }
+                    else
+                    {
+                        twiceChk = "!!!自动填报失败!!!";
+                    }
+                    switch (type)
+                    {
+                        case 0:
+                            LatestResult_Morning = DayTime + twiceChk;
+                            break;
+                        case 1:
+                            LatestResult_Moon = DayTime + twiceChk;
+                            break;
+                        case 2:
+                            LatestResult_Night = DayTime + twiceChk;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (type)
+                    {
+                        case 0:
+                            LatestResult_Morning = DayTime + "已填报, 无需重复提交";
+                            break;
+                        case 1:
+                            LatestResult_Moon = DayTime + "已填报, 无需重复提交";
+                            break;
+                        case 2:
+                            LatestResult_Night = DayTime + "已填报, 无需重复提交";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 是否自动午检
+        /// </summary>
+        public bool AutoMoon { get; set; }
+        /// <summary>
+        /// 是否自动晚检
+        /// </summary>
+        public bool AutoNight { get; set; }
+        /// <summary>
+        /// 是否跳过本条
+        /// </summary>
+        public bool Skip { get; set; }
+        /// <summary>
+        /// 晨检Payload
+        /// </summary>
+        public string Payload1 { get; set; }
+        /// <summary>
+        /// 午晚检Payload
+        /// </summary>
+        public string Payload2 { get; set; }
+        /// <summary>
+        /// 晨检执行结果
+        /// </summary>
+        private string _LatestResult_Morning;
+        public string LatestResult_Morning
+        {
+            get
+            {
+                return _LatestResult_Morning;
+            }
+            set
+            {
+                _LatestResult_Morning = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("LatestResult_Morning"));
+                }
+            }
+        }
+        /// <summary>
+        /// 午检执行结果
+        /// </summary>
+        private string _LatestResult_Moon;
+        public string LatestResult_Moon
+        {
+            get
+            {
+                return _LatestResult_Moon;
+            }
+            set
+            {
+                _LatestResult_Moon = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("LatestResult_Moon"));
+                }
+            }
+        }
+        /// <summary>
+        /// 晚检执行结果
+        /// </summary>
+        private string _LatestResult_Night;
+        public string LatestResult_Night
+        {
+            get
+            {
+                return _LatestResult_Night;
+            }
+            set
+            {
+                _LatestResult_Night = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("LatestResult_Night"));
+                }
+            }
+        }
+    }
+}
