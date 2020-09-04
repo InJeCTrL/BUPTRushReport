b15902978fa534c3941b320649c6cb0bd29dee25
InJeCTrL <dap1933@hotmail.com>
2020-09-04 19:46:33 +0800

    use font:'MS Outlook' to mask password column..lol

--- BUPTRushReport/MainWindow.xaml.cs
+++ BUPTRushReport/MainWindow.xaml.cs
@@ -4,8 +4,6 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Xml.Serialization;
 
 namespace BUPTRushReport
